92a3faaf4e58338d92cf19dd6875ea0c96bafda3
Tausif4171 <tausifkhan4171@gmail.com>
2025-06-29 14:42:29 +0530

    add WebSocket server for real-time keystroke monitoring

---
+++ server.js
@@ -0,0 +1,25 @@
+const keylogger = require("keylogger.js");
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+let clients = [];
+
+wss.on("connection", ws => {
+  clients.push(ws);
+  ws.on("close", () => clients = clients.filter(c => c !== ws));
+});
+
+let keystrokeCount = 0;
+
+keylogger.start((key, isKeyUp, keyCode) => {
+  if (!isKeyUp) {
+    keystrokeCount++;
+    console.log(`Key pressed: ${key}, total: ${keystrokeCount}`); // <--- Add this line
+    if (keystrokeCount % 2 === 0) {
+      clients.forEach(ws => ws.send("scroll"));
+    }
+  }
+});
+
+console.log("CodeScrollX server running on ws://localhost:8080");
+dddddd dd ddd dd d d  dd d d dd d  ddd  d d d d d
